7220d27cb61bdf8eca7d031d9f5c3121d743304b
USSURATONCACHI <redchindaniil@gmail.com>
2023-06-26 08:48:34 +0700

    Added zoom to photos. Added new hd photos

--- src/c2_practicum/scripts/image_picker.js
+++ src/c2_practicum/scripts/image_picker.js
@@ -1,21 +1,26 @@
-$('.preview-photos-list img').click(function(event) {
-    $('#main-photo-container img').attr('src', event.target.src);
+$('.preview-photos-list div').click(function(event) {
+    let image = $(this).children('img');
+
+    if (image.length <= 0) return;
+
+    $('#main-photo-container img').attr('src', image[0].src);
 });
 
-function onHover(x, y){
-    const image = $('#main-photo-container img');
+
+function onHover(x, y, container_id){
+    let image = $(`#${container_id} img`);
     const bgSize = 1200;
     const imageSize = image.width();
     const scale = bgSize / imageSize;
 
-    const container = $('.zoomed-image-container');
+    let container = $('.zoomed-image-container');
 
     const posX = -x * scale + container.width() / 2;
     const posY = -y * scale + container.height() / 2;
     container.css('background-position', `${posX}px ${posY}px`);
     container.css('background-image', `url(${image.attr('src')})`);
 
-    const overlay = $('#main-photo-container .overlay');
+    let overlay = $(`#${container_id} .overlay`);
     overlay.css('width', `${image.width() * container.width() / bgSize}px`);
     overlay.css('height', `${image.height() * container.height() / (image.height() * scale)}px`);
 
@@ -23,29 +28,38 @@ function onHover(x, y){
     overlay.css('top', `${y - overlay.height() / 2}px`);
 }
 
-$('#main-photo-container img').on('mousemove', event => onHover(event.offsetX, event.offsetY));
 
-
-$('#main-photo-container .overlay').on('mousemove', event => {
-    const overlay = $('#main-photo-container .overlay');
-    let pos = overlay.position();
-    onHover(event.offsetX + pos.left, event.offsetY + pos.top);
-});
-
-function onMouseEnterLeave() {
-    let hover = $('#main-photo-container img').is(":hover") || 
-        $('#main-photo-container .overlay').is(':hover');
+function onMouseEnterLeave(container_id) {
+    let hover = $(`#${container_id} img`).is(":hover") || 
+        $(`#${container_id} .overlay`).is(':hover');
 
     if (!hover) {
         $('.zoomed-image-container').css('display', 'none');
-        $('#main-photo-container .overlay').css('display', 'none');
+        $(`#${container_id} .overlay`).css('display', 'none');
     } else {
         $('.zoomed-image-container').css('display', 'inline-block');
-        $('#main-photo-container .overlay').css('display', 'inline-block');
+        $(`#${container_id} .overlay`).css('display', 'inline-block');
     }
 }
 
-$('#main-photo-container img').on('mouseleave', onMouseEnterLeave);
-$('#main-photo-container img').on('mouseenter', onMouseEnterLeave);
-$('#main-photo-container .overlay').on('mouseleave', onMouseEnterLeave);
-$('#main-photo-container .overlay').on('mouseenter', onMouseEnterLeave);
+function SetUpPhotoZoom(container_id) {
+
+    $(`#${container_id} img`).on('mousemove', event => onHover(event.offsetX, event.offsetY, container_id));
+
+
+    $(`#${container_id} .overlay`).on('mousemove', event => {
+        let overlay = $(`#${container_id} .overlay`);
+        let pos = overlay.position();
+        onHover(event.offsetX + pos.left, event.offsetY + pos.top, container_id);
+    });
+
+    $(`#${container_id} img`).on('mouseleave', event => onMouseEnterLeave(container_id));
+    $(`#${container_id} img`).on('mouseenter', event => onMouseEnterLeave(container_id));
+    $(`#${container_id} .overlay`).on('mouseleave', event => onMouseEnterLeave(container_id));
+    $(`#${container_id} .overlay`).on('mouseenter', event => onMouseEnterLeave(container_id));
+}
+
+SetUpPhotoZoom('main-photo-container');
+SetUpPhotoZoom('preview-small-1');
+SetUpPhotoZoom('preview-small-2');
+SetUpPhotoZoom('preview-small-3');
